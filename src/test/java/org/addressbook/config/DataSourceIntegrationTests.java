64627eeb8a88954022ab7c03b1b32fa0d128b18e
Aleksey Lisiikh <lizard5472@gmail.com>
2014-11-24 22:05:45 +0200

    Test for dataSource configuration

---
+++ src/test/java/org/addressbook/config/DataSourceIntegrationTests.java
@@ -0,0 +1,33 @@
+package org.addressbook.config;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * @author alisiikh.
+ */
+@ContextConfiguration(classes = {DataSourceConfig.class})
+@TransactionConfiguration(defaultRollback = true)
+@RunWith(SpringJUnit4ClassRunner.class)
+public class DataSourceIntegrationTests {
+
+    @Inject
+    private DataSource dataSource;
+
+    @Test
+    public void testConnectionIsAcquired() throws SQLException {
+        Connection connection = dataSource.getConnection();
+        Statement stmt = connection.createStatement();
+
+        assert stmt.execute("select 1");
+    }
+}
